ef92f2086562eea5d6f6ff6c4d9412c423a7f154
Kimonae <shinokasu@gmail.com>
2026-03-01 01:17:17 +0100

    Back-end okay,visual to end or create

--- model/user.model.php
+++ model/user.model.php
@@ -1,51 +1,41 @@
 <?php
-
 require_once('connexion.php');
+require_once('./user.php');
 
-require_once('../user.php');
 $usr = new User($db);
 $result = $usr->register();
 ?>
 
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <title>Inscription</title>
-</head>
-<body>
-
-<h1>Inscription</h1>
-
 <?php
 if ($result === true) {
-    echo "<p>Inscription réussie ! <a href='/login.php'>Connectez-vous</a></p>";
+    echo "<p class='text-success'>Inscription réussie !</p>";
 } elseif (is_string($result)) {
-    echo "<p style='color:red;'>$result</p>";
+    echo "<p class='text-danger'>$result</p>";
 }
 ?>
 
 <form action="" method="post">
-    <div>
-        <label for="name">Nom :</label>
-        <input type="text" id="name" name="name" required>
+    <div class="mb-3">
+        <label class="form-label">Nom</label>
+        <input type="text" name="name" class="form-control" required>
     </div>
-    <div>
-        <label for="firstname">Prénom :</label>
-        <input type="text" id="firstname" name="firstname" required>
-    </div>
-    <div>
-        <label for="mail">E-mail :</label>
-        <input type="email" id="mail" name="mail" required>
-    </div>
-    <div>
-        <label for="password">Mot de passe :</label>
-        <input type="password" id="password" name="password" required>
-    </div>
-    <div>
-        <input type="submit" value="S'inscrire">
-    </div>
-</form>
 
-</body>
-</html>
+    <div class="mb-3">
+        <label class="form-label">Prénom</label>
+        <input type="text" name="firstname" class="form-control" required>
+    </div>
+
+    <div class="mb-3">
+        <label class="form-label">E-mail</label>
+        <input type="email" name="mail" class="form-control" required>
+    </div>
+
+    <div class="mb-3">
+        <label class="form-label">Mot de passe</label>
+        <input type="password" name="password" class="form-control" required>
+    </div>
+
+    <button type="submit" class="btn btn-primary w-100">
+        S'inscrire
+    </button>
+</form>
